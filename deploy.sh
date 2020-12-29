1e9a277413045a0f1ab2d08fc9e32b7f2cffd4e8
Alexander Zafirov <alexander@zafirov.me->
2020-12-29 22:12:22 +0100

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -22,7 +22,10 @@ echo "Generating site"
 hugo
 
 echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+cd public && git add --all && git commit -m "Publishing to gh-pages (deploy.sh)"
 
 echo "Pushing to github"
+git push --all
+
+cd .. && git add --all && git commit -m "Publishing generated content (deploy.sh)"
 git push --all
